4c612dbc585ea132c1d2a55738b0ae2a58704f4a
mostafa alaeddin <mostafaalaeddin570@gmail.com>
2023-02-14 18:34:49 +0330

    added test class database

--- src/public/index.php
+++ src/public/index.php
@@ -1,3 +1,11 @@
-<?php 
+<?php
 
-    phpinfo();
+use App\Database\Database;
+
+require_once __DIR__ . DIRECTORY_SEPARATOR . '../vendor/autoload.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . '../App/Transaction.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . '../App/Database.php';
+
+
+
+$database = new Database();
